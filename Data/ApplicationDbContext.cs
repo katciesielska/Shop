0efb0760bfaae5beeb7bf873c0a849db8016f1ff
Katarzyna 138293 <pzx110261@student.poznan.merito.pl>
2025-01-28 04:09:04 +0100

    added register form input fields

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,5 +12,16 @@ namespace Shop.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Category>().HasData(
+            new Category{Id=1, Name="Bluzki i koszule", DisplayOrder=1},
+            new Category { Id= 2, Name= "Marynarki", DisplayOrder= 2},
+            new Category { Id = 3, Name="Spodnie", DisplayOrder = 3},
+            new Category { Id = 4, Name = "Spódnice", DisplayOrder = 4}
+            );
+        }
     }
 }
